2d5e9366f33ba7dcd0693bf530e850a69ccdaa35
LSL-Coding51 <168805232+LSL-Coding51@users.noreply.github.com>
2024-07-31 17:39:41 +1000

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-  const currentTheme = localStorage.getItem('theme');
-
-  if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-
-    if (currentTheme === 'dark') {
-      toggleSwitch.checked = true;
-    }
-  }
-
-  function switchTheme(e) {
-    if (e.target.checked) {
-      document.documentElement.setAttribute('data-theme', 'dark');
-      localStorage.setItem('theme', 'dark');
-    } else {
-      document.documentElement.setAttribute('data-theme', 'light');
-      localStorage.setItem('theme', 'light');
-    }
-  }
-
-  toggleSwitch.addEventListener('change', switchTheme);
-});
 document.getElementById("login").addEventListener('submit', function(event) {
   event.preventDefault();
 
