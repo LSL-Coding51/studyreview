4e7ab5cf4841a1e8599bb73e8f8c2e68115a7e45
LSL-Coding51 <168805232+LSL-Coding51@users.noreply.github.com>
2024-07-31 19:30:10 +1000

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('login-form');
+    const darkModeToggleBtn = document.getElementById('darkModeToggleBtn');
+    const body = document.body;
+   
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        body.classList.add('dark-mode');
+    }
 function toggleDarkMode() {
    var element = document.getElementById('html');
    element.classList.toggle("dark-mode");
+}
+function toggleDarkMode() {
+    const body = document.getElementById('html');
+    body.classList.toggle('dark-mode');
+
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        localStorage.removeItem('darkMode');
+    }
 }
