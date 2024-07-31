53d50447950aae871c5edf9d2b31ff747024bed0
LSL-Coding51 <168805232+LSL-Coding51@users.noreply.github.com>
2024-07-31 17:38:02 +1000

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+  const currentTheme = localStorage.getItem('theme');
+
+  if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+      toggleSwitch.checked = true;
+    }
+  }
+
+  function switchTheme(e) {
+    if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light');
+    }
+  }
+
+  toggleSwitch.addEventListener('change', switchTheme);
+});
 document.getElementById("login").addEventListener('submit', function(event) {
   event.preventDefault();
 
@@ -22,7 +46,6 @@ document.getElementById("login").addEventListener('submit', function(event) {
       document.getElementById('message').style.color = 'red';
     });
 });
-
 function toggleDarkMode() {
    var element = document.html;
    element.classList.toggle("dark-mode");
